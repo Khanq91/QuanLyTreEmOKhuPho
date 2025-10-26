c20f7e53118a5c6609763ac8164fa7943bf2ba8d
ASUS <nguyenhoangkhai2029@gmail.com>
2025-10-26 16:28:43 +0700

    Done mạnh thường quân

---
+++ QuanLyTreEmOKhuPho/Models/ManhThuongQuan/ThongKeManhThuongQuanDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyTreEmOKhuPho.Models.ManhThuongQuan
+{
+    public class ThongKeManhThuongQuanDTO
+    {
+        public int TongSoManhThuongQuan { get; set; }
+        public int SoToChuc { get; set; }
+        public int SoCaNhan { get; set; }
+        public decimal TongTienUngHo { get; set; }
+        public decimal TongTienUngHoThangNay { get; set; }
+        public int TongNhaTaiTroThuongXuyen { get; set; }
+    }
+}
