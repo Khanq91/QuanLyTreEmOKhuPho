99467dc0eae2b2b803fa1535a04c7e345cc99f5e
KimAnh <anhdangthikim438@gmail.com>
2025-11-10 08:54:11 +0700

    commit

---
+++ QuanLyTreEmOKhuPho/Models/SuKienDto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+namespace QuanLyTreEmOKhuPho.Models
+{
+
+    public class SuKienDetailVM
+    {
+        public int SuKienId { get; set; }
+        public string TenSuKien { get; set; }
+        public string MoTa { get; set; }
+        public string DiaDiem { get; set; }
+        public DateTime? NgayBatDau { get; set; }
+        public DateTime? NgayKetThuc { get; set; }
+        public string NguoiChiuTrachNhiem { get; set; }
+        public string KhuPho { get; set; }
+
+        public List<ThoiGianChiTietVM> ThoiGianChiTiet { get; set; } = new List<ThoiGianChiTietVM>();
+        public List<TietMucVM> TietMuc { get; set; } = new List<TietMucVM>();
+
+        public List<ChiPhiItemVM> ChiPhi { get; set; } = new List<ChiPhiItemVM>();
+    }
+
+    public class ThoiGianChiTietVM
+    {
+        public int ThoiGianChiTietSuKienId { get; set; }
+        public string MoTa { get; set; }
+        public DateTime? ThoiGianBatDau { get; set; }
+        public DateTime? ThoiGianKetThuc { get; set; }
+    }
+
+    public class TietMucVM
+    {
+        public string TenTietMuc { get; set; }
+        public string NguoiThucHien { get; set; }
+        public decimal? ChiPhiTietMuc { get; set; }
+
+        public DateTime? ThoiGian { get; set; }
+    }
+
+    public class ChiPhiItemVM
+    {
+        public string TenKhoanChi { get; set; }
+        public decimal SoTien { get; set; }
+        public string GhiChu { get; set; }
+
+        public List<ChiTietChiPhiVM> ChiTiet { get; set; } = new List<ChiTietChiPhiVM>();
+    }
+
+    public class ChiTietChiPhiVM
+    {
+        public string TenPhanQua { get; set; }
+        public string NguoiDaiDien { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+
+        // Optional: API computed field. You can set this server-side or compute client-side.
+        public decimal ThanhTien { get; set; }
+    }
+}
---
+++ QuanLyTreEmOKhuPho/Models/TreEmViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyTreEmOKhuPho.Models
+{
+    public class TreEmViewModel
+    {
+        public int TreEmId { get; set; }
+        public string HoTen { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string DanToc { get; set; }       // <-- Cần có
+        public string TonGiao { get; set; }      // <-- Cần có
+        public string Truong { get; set; }       // <-- Cần có
+        public string KhuPho { get; set; }       // <-- Cần có
+        public string Anh { get; set; }
+        public string TinhTrang { get; set; }
+
+        public List<string> HoanCanh { get; set; }
+        public List<PhuHuynhViewModel> PhuHuynh { get; set; }
+        public List<HocTapViewModel> HocTap { get; set; }
+        public List<HoTroViewModel> HoTro { get; set; }
+        public List<VanDongViewModel> VanDong { get; set; }
+    }
+    public class HocTapViewModel
+    {
+        public DateTime NgayCapNhat { get; set; }
+        public double DiemTrungBinh { get; set; }
+        public string XepLoai { get; set; }
+        public string HanhKiem { get; set; }
+        public string GhiChu { get; set; }
+    }
+    public class HoTroViewModel
+    {
+        public int HoTroId { get; set; }
+        public string LoaiHoTro { get; set; }
+        public string MoTa { get; set; }
+        public DateTime NgayCap { get; set; }
+        public List<string> File { get; set; }
+    }
+    public class VanDongViewModel
+    {
+        public DateTime NgayVanDong { get; set; }
+        public string LyDo { get; set; }
+        public string KetQua { get; set; }
+        public int SoLan { get; set; }
+    }
+    public class PhuHuynhViewModel
+    {
+        public string HoTen { get; set; }
+        public string Sdt { get; set; }
+        public string NgheNghiep { get; set; }
+        public string MoiQuanHe { get; set; }
+    }
+}
