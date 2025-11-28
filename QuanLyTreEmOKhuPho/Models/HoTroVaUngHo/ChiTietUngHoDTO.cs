997a1624a51e6d9d31f69760d83d1f678dbf27cb
ASUS <nguyenhoangkhai2029@gmail.com>
2025-11-28 18:15:36 +0700

    phân cụm

--- QuanLyTreEmOKhuPho/Models/HoTroVaUngHo/ChiTietUngHoDTO.cs
+++ QuanLyTreEmOKhuPho/Models/HoTroVaUngHo/ChiTietUngHoDTO.cs
@@ -12,7 +12,6 @@ namespace QuanLyTreEmOKhuPho.Models.HoTroVaUngHo
         public decimal PhanTrangHoanThanh { get; set; }
         public int SoLuongTreEmChuaNhanQua { get; set; }
         public int SoLuongTreEmDaNhan { get; set; }
-
         public string TenQua { get; set; }
         public string MoTa { get; set; }
         public int SoLuongTong { get; set; }
@@ -25,6 +24,7 @@ namespace QuanLyTreEmOKhuPho.Models.HoTroVaUngHo
         public string LoaiUngHo { get; set; }
         public string GhiChu { get; set; }
         public DateTime NgayUngHo { get; set; }
+        public string Anh { get; set; }
 
 
         public List<TreNhanQuaDTO> DanhSachTreNhan { get; set; }
---
+++ QuanLyTreEmOKhuPho/Models/NguoiDung/NguoiDungDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanLyTreEmAPI.DTOs.NguoiDung
+{
+    public class NguoiDungDTO
+    {
+        public int UserId { get; set; }
+        public string Ten { get; set; }
+        public string VaiTro { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
